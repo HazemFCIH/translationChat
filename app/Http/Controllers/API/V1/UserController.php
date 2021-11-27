5962f09a72bf949b5a865475ed0ca5c40a3e9054
Hazem Mohamed <hazem.fcih12@gmail.com>
2021-11-27 20:24:53 +0200

    changed get contacts api to success

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -10,7 +10,6 @@ class UserController extends Controller
 {
     public function contacts(Request $request)
     {
-        return $request->mobile_numbers;
 
         $contacts = User::whereIn('mobile_number',)->get();
         return response()->json(['contacts' => $contacts],200);
